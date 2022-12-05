5255f76a1d37df13f68ba292f978818faf585876
Eric K <erickirberger@gmail.com>
2022-12-04 19:49:26 -0500

    new clean version created, still need to crack the display

--- assets/js/script6.js
+++ assets/js/script6.js
@@ -1,13 +1,10 @@
-var saveBtn = document.querySelector('.saveBtn');
-var description = document.querySelector('description');
-
-// TODO: Add code to display the current date in the header of the page.
+// HEADER TIME DISPLAY FUNCTION
 function displayTime() {
 
   setInterval(function() {
-    var today = dayjs().format('dddd MMM, YYYY [-] h:mm:ss a');
-    $('#currentDay').text(today);
-    // $('#currentDay').text(today.format('dddd MMM, YYYY [-] h:mm:ss a'));
+
+    $('#currentDay').text((dayjs()).format('dddd MMM, YYYY [-] h:mm:ss a'));
+
   }, 1000);
 
 };
@@ -15,52 +12,50 @@ function displayTime() {
 // TODO: Add code to get any user input that was saved in localStorage and set
 // the values of the corresponding textarea elements. HINT: How can the id
 // attribute of each time-block be used to do this?
-function displayDescription() {
+// function displayDescription() {
 
-  $('time-block').each(function() {
-    var itemTime = localStorage.getItem(itemTime);
-    var storedDescription = localStorage.getItem(description);
-    var timeBlock = parseInt($(this).attr('id').split("hour-")[1]);
-  })
+//   $('time-block').each(function() {
+//     var itemTime = localStorage.getItem(itemTime);
+//     var storedDescription = localStorage.getItem(description);
+//     var timeBlock = parseInt($(this).attr('id').split("hour-")[1]);
+//   })
 
-  if (itemTime !== null && itemTime === timeBlock) {
-    $(this).children('.description').val(localStorage.getItem(itemTime));
-  }
+//   if (itemTime !== null && itemTime === timeBlock) {
+//     $(this).children('.description').val(localStorage.getItem(itemTime));
+//   }
 
-};
+// };
 
 displayTime();
-displayDescription();
+// displayDescription();
 
 $(function () {
 
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
+  var currentTime = parseInt(dayjs().format('HH'));
+
+  // TIME BLOCK COLOR CONDITIONAL
   $('.time-block').each(function() {
 
-    var currentTime = parseInt(dayjs().format('HH'));
     var itemTime = parseInt($(this).attr("id").split("hour-")[1]);
     console.log(itemTime, currentTime);
 
     if (itemTime > currentTime) {
-      $(this).addClass("future");  // is there a way to combine both removes?
-      $(this).removeClass("past");  // Or is there a way to make it only one specifically?
-      $(this).removeClass("present");
+      $(this).children('.description').addClass("future");  // is there a way to combine both removes?
+      $(this).children('.description').removeClass("past");  // Or is there a way to make it only one specifically?
+      $(this).children('.description').removeClass("present");
     } else if (itemTime < currentTime) {
-      $(this).addClass("past");
-      $(this).removeClass("present");
-      $(this).removeClass("future");
+      $(this).children('.description').addClass("past");
+      $(this).children('.description').removeClass("present");
+      $(this).children('.description').removeClass("future");
     } else {
-      $(this).addClass("present");
-      $(this).removeClass("past");
-      $(this).removeClass("future");
+      $(this).children('.description').addClass("present");
+      $(this).children('.description').removeClass("past");
+      $(this).children('.description').removeClass("future");
     }
 
   })
 
+  // SAVE BUTTON COLOR CONDITIONAL
   $('.saveBtn').each(function() {
 
     var itemTime = parseInt($(this).parent().attr("id").split("hour-")[1]);
@@ -82,12 +77,7 @@ $(function () {
 
   })
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // SAVE BUTTON FUNCTION
   $(".saveBtn").on("click", function () {
 
     var itemTime = parseInt($(this).parent().attr("id").split("hour-")[1]);
